67425fe92b052cdb8017879c7593848798df1854
Niyaj-Kumanali <iamniyazahmad777@gmail.com>
2023-08-29 22:17:49 +0530

    Updates

---
+++ precache-manifest.bd5f5803d3fd55aa5a45561f3772da8f.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e5a7c2fdcd741e3a6bf",
+    "url": "/myPortfolio.git/static/css/main.ba335b35.chunk.css"
+  },
+  {
+    "revision": "8e5a7c2fdcd741e3a6bf",
+    "url": "/myPortfolio.git/static/js/main.be5d7a05.chunk.js"
+  },
+  {
+    "revision": "338d4bc95824d78850f7",
+    "url": "/myPortfolio.git/static/js/runtime~main.cd51a686.js"
+  },
+  {
+    "revision": "11e7e98654485271cd5a",
+    "url": "/myPortfolio.git/static/js/2.c7f3c226.chunk.js"
+  },
+  {
+    "revision": "cbf000d54f62dd4e31635b75c58de518",
+    "url": "/myPortfolio.git/static/media/aqmenz.cbf000d5.png"
+  },
+  {
+    "revision": "fa31dcac353bf972e463f1d08a2febba",
+    "url": "/myPortfolio.git/static/media/mountain.fa31dcac.png"
+  },
+  {
+    "revision": "74e943d82452f81f6a0bb0b51ac6785d",
+    "url": "/myPortfolio.git/static/media/java.74e943d8.png"
+  },
+  {
+    "revision": "6606c48fbf49fc629449aa11170b8c1c",
+    "url": "/myPortfolio.git/static/media/python.6606c48f.png"
+  },
+  {
+    "revision": "69ae2e07ca6f84cc4659e82fb5e7e450",
+    "url": "/myPortfolio.git/static/media/html.69ae2e07.png"
+  },
+  {
+    "revision": "621f21fa891b48023ff7c4dff12c7aa0",
+    "url": "/myPortfolio.git/static/media/css.621f21fa.png"
+  },
+  {
+    "revision": "63cf9c6f8153aef1e884b7ae2815e06d",
+    "url": "/myPortfolio.git/static/media/js.63cf9c6f.png"
+  },
+  {
+    "revision": "6a1627f7ac104739620877d8493c3a7c",
+    "url": "/myPortfolio.git/static/media/machine-learning.6a1627f7.png"
+  },
+  {
+    "revision": "74f8e1a4fe8cf52f9e09765167adc70c",
+    "url": "/myPortfolio.git/static/media/reactjs.74f8e1a4.png"
+  },
+  {
+    "revision": "4c35a865810acc112d59b1648d24a3c7",
+    "url": "/myPortfolio.git/static/media/sql.4c35a865.png"
+  },
+  {
+    "revision": "afaa06055cd96215540d74b885ea4017",
+    "url": "/myPortfolio.git/static/media/profile.afaa0605.png"
+  },
+  {
+    "revision": "f34856a5dd982590ed0a1b208d6314dd",
+    "url": "/myPortfolio.git/static/media/hackerrank.f34856a5.png"
+  },
+  {
+    "revision": "c2b6ab35c68bd08a302f1ac0735e5482",
+    "url": "/myPortfolio.git/static/media/scaler.c2b6ab35.png"
+  },
+  {
+    "revision": "097e483a3effe10bef125bf813b69632",
+    "url": "/myPortfolio.git/static/media/quantumlearning.097e483a.png"
+  },
+  {
+    "revision": "0d4f5fd0e9975206a215ad6e96d490ea",
+    "url": "/myPortfolio.git/static/media/knowledge.0d4f5fd0.png"
+  },
+  {
+    "revision": "94b01077701f20b894bb0bc1bd535de9",
+    "url": "/myPortfolio.git/static/media/techfortune.94b01077.png"
+  },
+  {
+    "revision": "08bf268d351d179b591b5314ff53eada",
+    "url": "/myPortfolio.git/index.html"
+  }
+];
